3cf27df50fd289786fe5f4931b31d66ed507cfec
Shyam <shyam.nexus@gmail.com>
2025-09-29 05:45:18 +0530

    basic project created

---
+++ GccApplication1/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Device_Startup\startup_sam4e.c
+
+Device_Startup\system_sam4e.c
+
+main.c
+
+src\ASF\sam\utils\syscalls\gcc\syscalls.c
+
+src\ASF\common\utils\interrupt\interrupt_sam_nvic.c
+
+src\ASF\sam\drivers\can\can.c
+
---
+++ GccApplication1/main.c
@@ -0,0 +1,21 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 29-09-2025 05:23:00
+ * Author : conta
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
